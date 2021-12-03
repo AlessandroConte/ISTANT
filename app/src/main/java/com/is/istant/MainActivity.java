dafb8a43c024b16ac82d4fb4ff9583fa0f31282d
coonte99 <alessandro.conte@outlook.it>
2021-12-03 17:26:45 +0100

    Primo commit dell'app con solo la bottom navbar

---
+++ app/src/main/java/com/is/istant/MainActivity.java
@@ -0,0 +1,30 @@
+package com.is.istant;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = Navigation.findNavController(this, R.id.fragmentContainerView);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.grInfoFragment, R.id.loansFragment, R.id.searchFragment, R.id.activitiesFragment, R.id.settingsFragment).build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
