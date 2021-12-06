490a2c07010cc4e34fcdc8657a97ec0602268f8d
ThomasBabato <65108595+ThomasBabato@users.noreply.github.com>
2021-12-06 11:02:14 +0100

    Commit

--- app/src/main/java/com/is/istant/LoginActivity.java
+++ app/src/main/java/com/is/istant/LoginActivity.java
@@ -12,4 +12,10 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
+    public void onClickLoginBtn () {
+        setContentView(R.layout.activity_login);
+    }
+
+
+
 }
