0ce68f5bc3e2731f416148d7e569d49b3c7c669c
coonte99 <alessandro.conte@outlook.it>
2021-12-07 12:20:52 +0100

    Modifica toolbar e bottomnavbar. Riempimento schermate SettingsFragment e GrInfoFragment

--- app/src/main/java/com/is/istant/MainActivity.java
+++ app/src/main/java/com/is/istant/MainActivity.java
@@ -7,7 +7,10 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -19,12 +22,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView title = findViewById(R.id.toolbar_txt);
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
         NavController navController = Navigation.findNavController(this, R.id.fragmentContainerView);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.grInfoFragment, R.id.loansFragment, R.id.searchFragment, R.id.activitiesFragment, R.id.settingsFragment).build();
 
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
+
 }
