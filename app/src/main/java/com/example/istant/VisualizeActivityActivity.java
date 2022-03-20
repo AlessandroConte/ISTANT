70202c7bf3788676ef6eb0b7ee81d5a1ded9e177
coonte99 <alessandro.conte@outlook.it>
2022-03-20 15:55:33 +0100

    CutomListView
    CustomListView completa per lista di attività, funzionante ma da modificare graficamente.

---
+++ app/src/main/java/com/example/istant/VisualizeActivityActivity.java
@@ -0,0 +1,14 @@
+package com.example.istant;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VisualizeActivityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualize_activity);
+    }
+}
